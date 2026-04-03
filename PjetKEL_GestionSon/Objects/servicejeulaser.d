27013c08c91bbbf1586e81240308d72b819ae2bb
Clément bedour <bedour@insa-toulouse.fr>
2026-04-03 16:33:21 +0200

    Delete PjetKEL_GestionSon directory

--- PjetKEL_GestionSon/Objects/servicejeulaser.d
+++
@@ -1,6 +0,0 @@
-./objects/servicejeulaser.o: Driver\ServiceJeuLaser.c \
-  Driver\ServiceJeuLaser.h Driver\DriverJeuLaser.h \
-  C:\Users\Bedour\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.4.1\Device\Include\stm32f10x.h \
-  RTE\_Simulation\RTE_Components.h \
-  C:\Users\Bedour\AppData\Local\Arm\Packs\ARM\CMSIS\6.3.0\CMSIS\Core\Include\core_cm3.h \
-  C:\Users\Bedour\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.4.1\Device\Include\system_stm32f10x.h
